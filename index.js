9f4266d9184e3df0de6c8cc830cef0134a34a498
Martin Haynes <martin@dotmh.io>
2019-04-05 11:56:16 +0100

    adds content-type aware getter

--- index.js
+++ index.js
@@ -18,6 +18,28 @@ module.exports = {
         return this.rawPost;
     },
 
+    get post() {
+
+        let postData;
+
+        switch(this.event['Content-Type']) {
+            case "application/json":
+                postData = this.json;
+                break;
+            case "application/x-www-form-urlencoded":
+                postData = this.urlencoded;
+                break;
+            case "text/plain":
+                postData = this.plain;
+                break;
+            default:
+                postData = null;
+                break;
+        }
+
+        return postData;
+    },
+
     get rawPost() {
         const postData = this.event.body;
         if(!this.isPost() || (this.isPost() && !postData)) {
