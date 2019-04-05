44e37889f248ff5961d00491a1ef0d12c955be7f
Martin Haynes <martin@dotmh.io>
2019-04-05 16:21:57 +0100

    setting up testing

---
+++ spec/index.spec.js
@@ -0,0 +1,11 @@
+const Controller = require('@dotmh/lambda-controller');
+
+const post = require('..');
+
+function controllerFactory (event, callback) {
+    return new Controller(event, null, callback).add(post);
+}
+
+describe("Lambda Controller Post Extentions", function () {
+
+});
