86f352bf9a820037e0e9c7344c4a2706a358014b
Martin Haynes <martin.haynes@creative-assembly.com>
2019-07-04 15:32:05 +0100

    add pending tests for missing coverage

--- specs/index.spec.js
+++ specs/index.spec.js
@@ -1,8 +1,12 @@
 /* eslint-disable prefer-arrow-callback */
 const Controller = require("@dotmh/lambda-controller");
-const {expect} = require("chai");
+const {
+	expect
+} = require("chai");
 
-const {loadMock} = require("./helpers");
+const {
+	loadMock
+} = require("./helpers");
 const requestBody = require("..");
 
 class TestController extends Controller {
@@ -13,24 +17,47 @@ class TestController extends Controller {
 }
 
 describe("Lambda Controller Post Extentions", () => {
-	it("should correctly handle a valid JSON request body", function () {
-		const event = loadMock("valid-json");
-		const subject = new TestController(event, null, () => null);
+	describe("valid test", function () {
+		it("should correctly handle a valid JSON request body", function () {
+			const event = loadMock("valid-json");
+			const subject = new TestController(event, null, () => null);
 
-		expect(subject.requestBody).to.be.an("object").and.deep.equal({a: 1, b: 2, c: 3});
+			expect(subject.requestBody).to.be.an("object").and.deep.equal({
+				a: 1,
+				b: 2,
+				c: 3
+			});
+		});
+
+		it("should correctly handle a valid URL encoded request body", function () {
+			const event = loadMock("valid-urlencoded");
+			const subject = new TestController(event, null, () => null);
+
+			expect(subject.requestBody).to.be.an("object").and.deep.equal({
+				a: "1",
+				b: "2",
+				c: "3"
+			});
+		});
+
+		it("should correctly handle a valid Plain request body", () => {
+			const event = loadMock("valid-plain");
+			const subject = new TestController(event, null, () => null);
+
+			expect(subject.requestBody).to.be.an("string").and.equal(event.body);
+		});
+
+		it("should return null for a invalid/unknown MIME Type");
+		it("should return null for a non POST, PUT, PATCH request");
 	});
 
-	it("should correctly handle a valid URL encoded request body", function () {
-		const event = loadMock("valid-urlencoded");
-		const subject = new TestController(event, null, () => null);
-
-		expect(subject.requestBody).to.be.an("object").and.deep.equal({a: "1", b: "2", c: "3"});
+	describe("invalid", function () {
+		it("should return null on an invalid json");
 	});
 
-	it("should correctly handle a valid Plain request body", () => {
-		const event = loadMock("valid-plain");
-		const subject = new TestController(event, null, () => null);
-
-		expect(subject.requestBody).to.be.an("string").and.equal(event.body);
+	describe("short cut properties", function () {
+		it("should return the body data when using post()");
+		it("should return the body data when using put()");
+		it("should return the body data when using patch()");
 	});
 });
