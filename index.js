ad1f0589bc0142c906a4d28a52f1ee8169d4bd07
Martin Haynes <martin@dotmh.io>
2020-06-05 17:29:22 +0100

    fix docs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/***
+ /***
  *    Copyright 2020 DotMH Martin Haynes <oss@dotmh.io>
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,15 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
+ /**
+  * @module lambda-controller-request-body
+  */
 const querystring = require("querystring");
+
+/**
+ * A Plugin to allow you to handle the body of requests
+ * */
 
 module.exports = {
 	/**
